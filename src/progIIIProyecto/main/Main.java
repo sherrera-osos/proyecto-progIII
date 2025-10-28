fc3ede36c278cfdffcfab555452ad2ffb6877954
Asier Zamalloa <asier.zamalloa@opendeusto.es>
2025-10-28 12:38:50 +0100

    prueba Asier

--- src/progIIIProyecto/main/Main.java
+++ src/progIIIProyecto/main/Main.java
@@ -5,7 +5,6 @@ import progIIIProyecto.ventana.VentanaPrincipal;
 public class Main {
 	public static void main(String[] args) {
 		new VentanaPrincipal();
-		System.out.println("Hola Mundo");
 	}
 
 }
