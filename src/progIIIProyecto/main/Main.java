c706b3f334740b1911d38a6187d4882d54d33bb1
Samuel Herrera <s.herrera@opendeusto.es>
2025-10-28 12:20:42 +0100

    Inicializo el Main y creo la ventana principal provisional.

--- src/progIIIProyecto/main/Main.java
+++ src/progIIIProyecto/main/Main.java
@@ -1,5 +1,10 @@
 package progIIIProyecto.main;
 
+import progIIIProyecto.ventana.VentanaPrincipal;
+
 public class Main {
+	public static void main(String[] args) {
+		new VentanaPrincipal();
+	}
 
 }
---
+++ src/progIIIProyecto/ventana/VentanaPrincipal.java
@@ -0,0 +1,20 @@
+package progIIIProyecto.ventana;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	public VentanaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Proyecto ProgIII"); //NOMBRE PROVISIONAL
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		
+		
+		
+		setVisible(true);
+	}
+
+}
