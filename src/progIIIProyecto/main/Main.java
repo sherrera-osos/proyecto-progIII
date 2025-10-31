10e7f402bb1ed6df8924f9399209c67ec7e38a4a
perez.markel <perez.markel@BI24-60F0D07600>
2025-10-31 09:41:54 +0100

    Cambio

--- src/progIIIProyecto/main/Main.java
+++ src/progIIIProyecto/main/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		new VentanaPrincipal();
 		System.out.println("Cambio");
+		System.out.println("Hola");
+		System.out.println("Hola");
 	}
 
 }
