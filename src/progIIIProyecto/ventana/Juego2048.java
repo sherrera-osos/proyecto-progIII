bfe18e8e825b6a29b3c3025278400d301806ff31
perez.markel <perez.markel@10.164.54.12>
2025-11-26 13:34:06 +0100

    He avanzado con el juego de 2048

--- src/progIIIProyecto/ventana/Juego2048.java
+++ src/progIIIProyecto/ventana/Juego2048.java
@@ -3,8 +3,11 @@ package progIIIProyecto.ventana;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.datatransfer.Clipboard;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -13,7 +16,7 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Juego2048  extends JFrame{
-
+	
 	/**
 	 * 
 	 */
@@ -22,7 +25,8 @@ public class Juego2048  extends JFrame{
 	private JLabel puntuacion;
 	private JPanel matriz [][];
 	private JLabel numeros[][];
-	
+    private int tablero[][] = new int[4][4];
+    	
 	public Juego2048() {
 		setTitle("2048");
 		setBounds(500,200, 400, 400);
@@ -47,7 +51,8 @@ public class Juego2048  extends JFrame{
 				matriz[i][j].setOpaque(true);
 				matriz[i][j].setBackground(new Color(197,183,170));
 				
-				numeros[i][j] = new JLabel("8");
+				numeros[i][j] = new JLabel("0");
+				numeros[i][j].setFont(new Font("Serif",Font.BOLD,27));
 				numeros[i][j].setHorizontalAlignment(SwingConstants.CENTER);
 				
 		        matriz[i][j].add(numeros[i][j]);
@@ -57,16 +62,19 @@ public class Juego2048  extends JFrame{
 
 			}
 		}
+		generarNuevoNumero();
+		actualizarPantalla();
+		
 		setVisible(true);
 
 		
 	}
 	public void actualizarColorCelda(int i, int j) {
-	    int valor = Integer.parseInt(numeros[i][j].getText());
+	    int valor = tablero[i][j];
 
 	    switch (valor) {
 	        case 0:
-	            matriz[i][j].setBackground(new Color(197,183,170));
+	            matriz[i][j].setBackground(new Color(205,193,180));
 	            numeros[i][j].setForeground(Color.BLACK);
 	            break;
 	        case 2:
@@ -81,9 +89,93 @@ public class Juego2048  extends JFrame{
 	            matriz[i][j].setBackground(new Color(242,177,121));
 	            numeros[i][j].setForeground(Color.WHITE);
 	            break;
-
+	        case 16:
+	            matriz[i][j].setBackground(new Color(245,149,99));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 32:
+	            matriz[i][j].setBackground(new Color(246,124,95));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 64:
+	            matriz[i][j].setBackground(new Color(246,94,59));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 128:
+	            matriz[i][j].setBackground(new Color(237,207,114));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 256:
+	            matriz[i][j].setBackground(new Color(237,204,97));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 512:
+	            matriz[i][j].setBackground(new Color(237,200,80));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 1024:
+	            matriz[i][j].setBackground(new Color(237,197,63));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        case 2048:
+	            matriz[i][j].setBackground(new Color(237,194,46));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
+	        default: // Para valores mayores
+	            matriz[i][j].setBackground(new Color(60,58,50));
+	            numeros[i][j].setForeground(Color.WHITE);
+	            break;
 	    }
 	}
+	
+	public boolean estaLleno() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (tablero[i][j]==0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public void generarNuevoNumero() {
+		if (estaLleno()) {
+			return;
+		}
+		List<int[]> vacias = new ArrayList<int[]>();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (tablero[i][j]==0) {
+					vacias.add(new int[] {i,j});
+				}
+			}
+		}
+		
+		int [] pos = vacias.get((int) (Math.random()* vacias.size())); // Hay que hacer un casteo
+		int j = pos[1];
+		int i = pos[0];
+		double probabilidad = Math.random(); // número aleatorio entre 0 y 1
+		if (probabilidad < 0.9) { // el 90% de los casos, siempre aparece el numero 2 y no el 4
+		    tablero[i][j] = 2;
+		} else {
+		    tablero[i][j] = 4;
+		}
+	}
+	
+	public void actualizarPantalla () {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (tablero[i][j] == 0) {
+				    numeros[i][j].setText("");
+				} else {
+				    numeros[i][j].setText(String.valueOf(tablero[i][j]));
+				}
+				actualizarColorCelda(i, j);
+			}
+			
+		}
+	}	
 
 
 	public static void main(String[] args) {
